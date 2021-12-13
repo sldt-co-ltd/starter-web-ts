b1b99f686adbd62352841d70193007f010db7cf2
Alex Lee <iasandcb@gmail.com>
2021-12-13 10:52:56 +0900

    Refactor with function composition

--- test/randomizer.test.ts
+++ test/randomizer.test.ts
@@ -1,15 +1,10 @@
-import { generateRandomInt } from '@/utils/randomizer';
-
-const crypto = require('crypto');
-const randomizer = crypto.randomFillSync;
-// const crypto = window.crypto
-// const randomizer = crypto.getRandomValues;
+import { generateInt } from '@/utils/randomizer';
 
 describe('randomizer', () => {
   it('generates random integers', () => {
   
     for (let i = 0; i < 10000; i++) {
-      const randomInteger = generateRandomInt(1, 100, randomizer);
+      const randomInteger = generateInt(1, 100);
       expect(randomInteger).toBeGreaterThanOrEqual(1);
       expect(randomInteger).toBeLessThan(100);
     }
--- utils/randomizer.ts
+++ utils/randomizer.ts
@@ -1,9 +1,24 @@
 export const generateRandomInt: ((min: number, max: number, randomizer: (any: any) => void) => number) 
-  = function(min: number, max: number, randomizer: (any: any) => void) {
+  = function(min: number, max: number, randomizer: (any: any) => void): number {
   const bottom: number = Math.ceil(min);
   const top: number = Math.floor(max);
   const width: number = max - min;
   let array = new Uint32Array(1);
   randomizer(array);
   return array[0] % width + min; //최댓값은 제외, 최솟값은 포함
+}
+
+export const provideRandomizer: (() => ((any: any) => void)) = function(): ((any: any) => void) {
+  const crypto = window.crypto
+  if (crypto !== undefined) {
+    return crypto.getRandomValues;
+  }
+
+  const nodeCrypto = require('crypto');
+  return nodeCrypto.randomFillSync;
+}
+
+export const generateInt: (min: number, max: number) => number 
+  = function(min: number, max: number): number {
+  return generateRandomInt(min, max, provideRandomizer());
 }
